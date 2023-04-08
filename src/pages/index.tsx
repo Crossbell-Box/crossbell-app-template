8f8a752b774652cff3540ee07ccc3ed14362dadd
Runjuu <hi@runjuu.com>
2023-04-07 21:35:51 -0700

    feat: add `Header`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,31 @@ import {
   useDisconnectModal,
   useAccountState,
 } from "@crossbell/connect-kit";
-import { CharacterAvatar } from "@crossbell/ui";
+import { CharacterAvatar, useWeb2Url } from "@crossbell/ui";
 import { extractCharacterName } from "@crossbell/util-metadata";
 
 export default function IndexPage() {
+  return (
+    <div>
+      <Header />
+      <Connection />
+    </div>
+  );
+}
+
+function Header() {
+  const logoUrl = useWeb2Url(
+    "ipfs://bafkreidtjso4rkvefy2yd63p2r2fze74yxadnpc5wbrgrz5yunkd6tnxo4"
+  );
+
+  return (
+    <div className="flex items-center justify-center p-6">
+      <img src={logoUrl} alt="Crossbell Logo" />
+    </div>
+  );
+}
+
+function Connection() {
   const connectModal = useConnectModal();
   const disconnectModal = useDisconnectModal();
   const account = useAccountState((s) => s.computed.account);
