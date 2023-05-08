f560f3c0713d42a69aa39668d231e271e564bd90
Runjuu <hi@runjuu.com>
2023-05-08 11:06:29 -0700

    chore: update deps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const { withIpfsGateway } = require("@crossbell/ipfs-gateway-next");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // https://github.com/WalletConnect/walletconnect-monorepo/blob/7716e164281c2f531145d682c3658f761fa0a823/providers/universal-provider/src/utils/deepLinks.ts#L39
+    // @walletconnect/universal-provider imports react-native conditionally.
+    // Since this is a NextJS app, simply mark it as external to avoid the webpack bundling warning.
+    config.externals.push("react-native");
+
+    return config;
+  },
+};
 
 module.exports = withIpfsGateway(nextConfig);
